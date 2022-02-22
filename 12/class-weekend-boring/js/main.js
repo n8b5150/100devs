23c3133e1615e4ecb86d3b0f468c4bdafe303c47
n8b5150 <nate.burks@gmail.com>
2022-02-22 18:23:28 -0500

    minor bug fixes and feature additions.

--- 12/class-weekend-boring/js/main.js
+++ 12/class-weekend-boring/js/main.js
@@ -4,7 +4,7 @@ document.querySelector('#check').addEventListener('click',whatDay)
 
 function whatDay() {
     const day = document.querySelector('#day').value.toLowerCase()
-    if (day === 'tuesday' || day === 'thurdsay') {
+    if (day === 'tuesday' || day === 'thursday') {
         document.querySelector('h2').innerText = 'Yay! Class Day!!'
     }else if (day === 'monday' || day === 'wednesday') {
         document.querySelector('h2').innerText = 'bbbboooooorrrrrrrrrrrinnnngggggggg'
--- 12/simple-calculator/js/main.js
+++ 12/simple-calculator/js/main.js
@@ -1,18 +1,13 @@
 let num = 0
 
-// document.querySelector('#pumpkin').addEventListener('click',num+=0)
-// document.querySelector('#dominosPizza').addEventListener('click',num+=3)
-// document.querySelector('#zebra').addEventListener('click',num+=9)
-// document.querySelector('#cant').addEventListener('click',num-=2)
-// document.querySelector('#reset').addEventListener('click',num=0)
-document.querySelector('#pumpkin').addEventListener('click',addZero)
+document.querySelector('#pumpkin').addEventListener('click',addOne)
 document.querySelector('#dominosPizza').addEventListener('click',addThree)
 document.querySelector('#zebra').addEventListener('click',addNine)
 document.querySelector('#cant').addEventListener('click',subTwo)
 document.querySelector('#reset').addEventListener('click',reset)
 
-function addZero() {
-    num += 0
+function addOne() {
+    num += 1
     document.querySelector('#placeToPutResult').innerText = num
 }
 function addThree() {
