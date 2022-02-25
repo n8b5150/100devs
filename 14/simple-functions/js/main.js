28c58a3ecaa35a44794cdcb15ed7e1164f78780f
n8b5150 <nate.burks@gmail.com>
2022-02-24 21:41:50 -0500

    added null check to temp converter, for loop to simple functions

--- 14/simple-functions/js/main.js
+++ 14/simple-functions/js/main.js
@@ -1,17 +1,20 @@
 //create a function that adds two numbers and alerts the sum
-function addTwo(n1,n2) {
-    alert(n1+n2)
+function addTwo( n1 , n2 ) {
+    alert( n1 + n2 )
 }
-addTwo(5,6)
+addTwo( 5 , 6 )
 
 //create a function that multiplys three numbers and console logs the product
-function multThree(n3,n4,n5){
-    console.log(n4*n5*n3)
+function multThree( n3 , n4 , n5 ){
+    for ( i = 0 ; i <= 12 ; i++ ){
+        console.log( n4++ * n5++ *n3++ )
+        // add one to each and do it again
+    }
 }
-multThree(2,3,4)
+multThree( 2 , 3 , 4 )
 
-//create a function that divides two numbers and returns the ???
-function divTwo(n6,n7){
-    return(n6/n7)
+//create a function that divides two numbers and returns the ???quotient
+function divTwo( n6 , n7 ) {
+    return( n6 / n7 )
 }
-console.log(divTwo(12,6))
+console.log( divTwo( 12 , 6 ) )
--- 14/temperature-converter/js/main.js
+++ 14/temperature-converter/js/main.js
@@ -4,16 +4,19 @@ document.querySelector('#convert').addEventListener('click',convert)
 
 
 function convert() {
-    let temp = Number(document.querySelector('#temperature').value)
+    let temp = document.querySelector('#temperature').value
     let answer
-    if (document.querySelector('#celsRadio').checked === true) {
+    if (temp == '') {
+        alert("Please enter a temperature!")
+    }
+    else if (document.querySelector('#celsRadio').checked === true) {
         answer = 1.8 * temp + 32
         answer = answer.toFixed(2)
-        document.querySelector("#answer").innerText = answer + " degrees F"
+        document.querySelector("#answer").innerText = answer + " °F"
     }else if (document.querySelector('#fahrRadio').checked === true) {
         answer = 5 / 9 * (temp - 32)
         answer = answer.toFixed(2)
-        document.querySelector("#answer").innerText = answer + " degrees C"
+        document.querySelector("#answer").innerText = answer + " °C"
     }else {
         alert("Please select a conversion option!")
     }
