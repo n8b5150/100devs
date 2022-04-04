0dc7e462ed4f084c5a497d8712792111a3975e1f
n8b5150 <nate.burks@gmail.com>
2022-04-04 15:29:27 -0400

    codewars

--- codewars/gravity-flip.js
+++ codewars/gravity-flip.js
@@ -18,3 +18,10 @@
 // Examples (input -> output:
 // * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 // * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+function flip(dir, arr){
+    return dir = 'R' ? arr.sort( (a,b) => a - b) : arr.sort( (a,b) => b - a )
+}
+
+console.log(flip('R',[3, 2, 1, 2]))
+console.log(flip('L',[1, 4, 5, 3, 5 ]))
