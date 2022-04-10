8e4d655afc5b42dbd72ef1829ac1c1292d04930b
Nathan Burks <87131703+n8b5150@users.noreply.github.com>
2022-04-10 09:18:32 -0400

    codewars, rpsls styling and layout

---
+++ codewars/square-root-or-square.js
@@ -0,0 +1,24 @@
+// #To square(root) or not to square(root)
+
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// The input array will always contain only positive numbers and will never be empty or null.
+
+// The input array should not be modified!
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+function squareOrNot(arr){
+    let newArr = arr.map( e => {
+        return Math.sqrt(e) % 1 == 0 ? Math.sqrt(e) : e**2
+    })
+    return newArr
+}
+
+console.table(squareOrNot([1,2,3,4,5,6,7,8,9,21,25,100,1000,144]))
