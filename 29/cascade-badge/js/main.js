895edb1d6764ec9d6bd7a1029b611d2cfcfbc4de
Nathan Burks <87131703+n8b5150@users.noreply.github.com>
2022-04-26 21:49:56 -0400

    codewars

--- 29/cascade-badge/js/main.js
+++ 29/cascade-badge/js/main.js
@@ -17,13 +17,15 @@ console.log(squareSumCubeSum([1,2,3,4,5,6],[1,2,3]))
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
-const indexModuloZero = (arr) => {
-    let newArr = []
-    arr.forEach( (e,i) => {
-        if ( e%i === 0 ) newArr.push(e)    
-    })
-    return newArr
-}
+// const indexModuloZero = (arr) => {
+//     let newArr = []
+//     arr.forEach( (e,i) => {
+//         if ( e%i === 0 ) newArr.push(e)    
+//     })
+//     return newArr
+// }
+
+const indexModuloZero = (arr) => arr.filter( (e,i) => e%i === 0)
 
 console.log(indexModuloZero([68, -1, 1, -7, 10, 10]), [-1, 10])
 console.log(indexModuloZero([22, -6, 32, 82, 9, 25]),[-6, 32, 25])
