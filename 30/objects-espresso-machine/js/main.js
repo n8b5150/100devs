6a488d767c5a1013bd256ecba87a2b460be089f0
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-04-28 18:53:47 -0400

    class 30 activities

--- 30/objects-espresso-machine/js/main.js
+++ 30/objects-espresso-machine/js/main.js
@@ -1,1 +1,26 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor(manufacturer,model,temperature,pressure) {
+      this.manufacturer = manufacturer
+      this.model = model
+      this.temperature = temperature
+      this.pressure = pressure
+    }
+    makeEspresso(){
+        console.log('gurgling noises and steam')
+    }
+    frothMilk(){
+        console.log('frothing milk')
+    }
+    beingExpensive(){
+        console.log("I'm expensive")
+    }
+  }
+
+  let bravo = new EspressoMachine('Bravo','Model 1','100F','25psig')
+
+  console.table(bravo)
+  bravo.makeEspresso()
+  bravo.frothMilk()
+  bravo.beingExpensive()
