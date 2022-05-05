e3eeac6de608f45bf83f7cf1161edc8a72a6eee6
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-05-05 14:00:14 -0400

    codewars

---
+++ codewars/5kyu/primes-in-numbers.js
@@ -0,0 +1,5 @@
+// 5 kyu
+// Primes in numbers
+// Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
+
+//  "(p1**n1)(p2**n2)...(pk**nk)"
