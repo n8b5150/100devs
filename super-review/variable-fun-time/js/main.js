87b1c780e393ee005dee3076ca6e50eab2ad3a5a
n8b5150 <nate.burks@gmail.com>
2022-05-15 14:05:22 -0400

    added super review materials

--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -1,29 +1,18 @@
 //--- Easy
 //create a variable and assign it a number
-let num = 7
 
 //minus 10 from that number
-num -=10
 
 //print that number to the console
-console.log(num)
 
 //--- Medium
 //create a variable that holds a value from the input
-let numInput = Number(document.querySelector('input').value)
 
 //add 25 to that number
-numInput += 25
 
 //alert that number
-alert(numInput)
 
 //--- Hard
 //create a variable that holds the h1
-const hOne = document.querySelector('h1')
 
 //add an event listener to that element that console logs the sum of the two previous variables
-hOne.addEventListener('click', sum => {
-    sum = num + numInput
-    console.log(sum)
-})
