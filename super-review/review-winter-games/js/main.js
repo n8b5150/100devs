76079f167115eeba178bd5a1b5c3dcb0fe6dc773
Nathan Burks <87131703+n8b5150@users.noreply.github.com>
2022-05-17 07:15:13 -0400

    super review activities

--- super-review/review-winter-games/js/main.js
+++ super-review/review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function evenNums(arr){
+    let evens = []
+    arr.forEach( e => {
+        if (e%2 === 0) {
+            evens.push(e)
+        }
+    })
+    console.log(evens)
+}
+
+evenNums([1,2,4,3,5,6,7,8,9,11,113,56])
