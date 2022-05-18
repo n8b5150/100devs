3825e2bcbe09a38621325d00b81e664489cfd52b
n8b5150 <nate.burks@gmail.com>
2022-05-17 21:17:48 -0400

    super review activities

--- super-review/objects-listen-to-your-heart/js/main.js
+++ super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+
+const stopwatch = {}
+
+stopwatch.color = 'black'
+stopwatch.brand = 'nike'
+stopwatch.smell = 'new'
+stopwatch.shape = 'round'
+
+stopwatch.start = function() {
+    console.log('runnnn')
+}
+stopwatch.stop = function() {
+    console.log('stop')
+}
+stopwatch.reset = function() {
+    console.log('reset')
+}
--- super-review/objects-street-fighter/js/main.js
+++ super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name,specialMove,health,anger){
+    this.name = name
+    this.specialMove = specialMove
+    this.health = health
+    this.anger = anger
+
+    this.kick = function() {
+        alert('kick')
+    }
+    this.punch = function() {
+        alert('punch')
+    }
+}
+
+let ryu = new StreetFighter('Ryu','Hadoken',100,30)
+console.log(ryu)
+ryu.kick()
+ryu.punch()
+ryu.headband = 'black'
+
+ryu.headband
