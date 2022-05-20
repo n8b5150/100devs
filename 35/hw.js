f5269f51bf475e92cd913f634649e838a1dfdc2b
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-05-20 11:02:02 -0400

    added try catch to class 35 hw

--- 35/hw.js
+++ 35/hw.js
@@ -1,8 +1,13 @@
 //Code 08
 // HW - add try catch
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+    try {
+        const res = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await res.json()
+        console.log(data.message)
+        document.querySelector('img').src = data.message
+    } catch(err) {
+        console.error(err)
+    }
+}
+getACuteDogPhoto()
