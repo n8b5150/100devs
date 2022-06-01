336ecc2c5e4dc825ff26608c3d3b49a81aa5871d
n8b5150 <nate.burks@gmail.com>
2022-05-31 22:06:09 -0400

    class 38 activities

---
+++ 38/api-project/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+        },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennet',
+        'birthLocation': 'Chicago, Illinois'
+        },
+    'dylan': {
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+        },
+    }
+
+app.get('/', (request,response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request,response)=>{
+    const rapperName = request.params.rapperName.toLowerCase()
+    if (rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(process.env.port || PORT, ()=>{
+    console.log(`Server running on ${PORT}`)
+})
+
+// heroku commands:
+// https://www.heroku.com/
+// https://rap-names-api-fun.herokuapp.com/
+// heroku login -i
+// heroku create simple-rap-api
+// echo "web: node server.js" > Procfile
+// git add . 
+// git commit -m "changes"
+// git push heroku main
